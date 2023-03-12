c3b68cfac2e564c178a1601f1a79993b44bbdf8b
ssarcandy <ssarcandy@gmail.com>
2023-03-12 10:38:37 +0000

    test: add more testcases

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
 #include "http/Http.h"
+#include "simdjson/singleheader/simdjson.h"
+
+using namespace simdjson;
 
 namespace {
 
@@ -15,7 +18,7 @@ TEST(Http, check_handle_count) {
 
   EXPECT_GT(http.pending_requests(), 0);
 }
-/*
+
 TEST(Http, get) {
   const vector<string> urls = {
       "https://1.1.1.1",
@@ -95,7 +98,7 @@ TEST(Http, post_with_header) {
       const auto d = parser.parse(res.data_);
       EXPECT_EQ("12345", d["headers"]["x-mbx-apikey"].get_string().value());
     })
-    .set_headers({fmt::format("X-MBX-APIKEY: {}", "12345")})
+    .set_headers({"X-MBX-APIKEY: 12345"})
     .post();
   // clang-format off
 
@@ -170,31 +173,4 @@ TEST(Http, put) {
   } while (http.pending_requests());
 }
 
-TEST(Http, benchmark) {
-  const string url = "https://fapi.binance.com/fapi/v1/time";
-  const int REQUESTS = 100;
-  Timer::start("x");
-
-  Http http;
-  size_t failed = 0;
-  for (auto i = 0; i < REQUESTS; ++i) {
-    // clang-format off
-    http
-      .request(url, [&](const ResponseInfo& res) {
-        dom::parser parser;
-        const auto x = parser.parse(res.data_);
-        if (x.error()) ++failed;
-      })
-      .get();
-    // clang-format on
-  }
-  do {
-    http.poll();
-  } while (http.pending_requests());
-
-  const auto t = Timer::end("x");
-  fmt::print(">> Requests per second: {:4.2f} [#/sec]\n", REQUESTS / t);
-  EXPECT_EQ(0, failed);
-  EXPECT_LT(t, 0.5);
-}*/
 }  // namespace
